fb5343519adca223947562fdb516dc302a16bc7a
dragungX <gunxag@tutamail.com>
2024-11-13 20:39:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const agung = document.getElementById("layar");
+const hesti = agung.getContext("2d");
+
+agung.width = window.innerWidth;
+agung.height = window.innerHeight;
+
+hesti.fillStyle = "white";
+hesti.fillRect(50,50,500,1500);
