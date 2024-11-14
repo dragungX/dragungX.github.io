2a6feaa674328ddbcafd2a33e088a353331e033e
dragungX <gunxag@tutamail.com>
2024-11-13 20:50:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const hesti = agung.getContext("2d");
 agung.width = window.innerWidth;
 agung.height = window.innerHeight;
 
-hesti.fillStyle = "blue";
+hesti.fillStyle = "white";
 hesti.fillRect(50,50,50,50);
 
 hesti.beginPath();
+hesti.fill();
 hesti.arc(200,200,100,0,Math.PI*2);
